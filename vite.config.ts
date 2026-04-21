713581d2b7d6a49751ea2dd4487ae26d1fdbe53e
shinyei <sophia.sy.wu@gmail.com>
2026-04-21 16:19:45 +0800

    chore: update vite configuration settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 確保使用相對路徑，這樣不論 GitHub Pages 子網址長怎樣都能抓到資源
+  base: '/pixel-quiz-game/', // 確保使用正確的 repository 名稱作為基底路徑
 });
